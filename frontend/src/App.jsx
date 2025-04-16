110987de7cce696f2bc7ab2c8d306e62201d2443
Rishu5110 <157588332+Rishu5110@users.noreply.github.com>
2025-04-16 23:15:42 +0530

    Folder Structure Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App()
+{
+    return (
+        <div className="App">
+            <h1>Eco-eye</h1>
+            <p>Welcome to the Eco-eye application!</p>
+        </div>
+    );
+}
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Services</li>
+        <li>Blog</li>
+      </ul>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
