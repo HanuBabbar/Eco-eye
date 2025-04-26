9af93a4145191f1570b5488aaa81d89a670610f1
Karan Mittal <Karanmittal662@gmail.com>
2025-04-26 20:53:46 +0530

    complaint backend

---
+++ backend/src/Models/ComplaintModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const User = require('../Models/RequestModel');
+ 
+const complaintSchema = new mongoose.Schema({
+    complaintLocation:{
+        type: String,
+        required: true
+    },
+    complaintDate:{
+        type:Date,
+        default: Date.now,
+    },
+    complaintDescription:{
+        type: String,
+        required: true
+    },
+    complaintStatus:{
+        type: Boolean,
+        default: false,
+    },
+    complaintImage:{
+        type: String,
+        required: true
+    },
+    complaintUser:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Complaint', complaintSchema);
--- backend/src/controllers/ImageUpload.js
+++ backend/src/controllers/ImageUpload.js
@@ -1,11 +1,31 @@
 const {uploadOnCloudinary} = require('../utils/fileUpload');
+const ComplaintModel = require('../Models/ComplaintModel');
 
 exports.uploadImage =async (req, res) => {
-    const { description, location } = req.body; // Access additional fields
+    const {complaintLocation, complaintDate, complaintDescription, complaintStatus, complaintImage} = req.body; // Access additional fields
     const file = req.file; // Access the uploaded file
     if (!file) {
         return res.status(400).json({ success: false, message: 'No file uploaded.' });
     }
+    // Validate the required fields
+    if (!complaintLocation || !complaintDescription || !complaintImage || !complaintStatus || !complaintDate) {
+        console.log("Complaint Missing Fields");
+        return res.status(400).json({ success: false, message: 'All fields are required.' });
+    }
+    // Push the data to databse
+    await ComplaintModel.create({
+        complaintLocation,
+        complaintDate,
+        complaintDescription,
+        complaintStatus,
+        complaintImage: file.path, // Store the file path in the database
+    }).then(() => {
+        console.log("Complaint Created");
+    }).catch((err) => {
+        console.log(err);
+    });
+
+
     // Upload the file to Cloudinary
     const cloudinaryResponse = await uploadOnCloudinary(file.path);
     if (!cloudinaryResponse) {
